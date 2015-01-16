5e103a683810e27990c5e4a6721fd5b23bd75791
parbhat <parbhatpuri17@gmail.com>
2015-01-16 18:43:57 +0530

    project initialized with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resurgenceWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the resurgenceWebsiteApp
+ */
+angular.module('resurgenceWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
