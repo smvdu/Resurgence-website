d2fca69523845c66cf420702f9230849bd0799ce
codingeass <amandeeptheviper@gmail.com>
2015-03-25 18:27:37 +0530

    inter university section removed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,9 @@ window.onload=function(e){
 	{
 		document.getElementById("event_list").setAttribute("style", " float: none;width: 100%;height: auto;overflow-y:visible;");
 	}
+	document.getElementById("selection").value="intra";
+	document.getElementById("third").style.display="none";	
+	document.getElementById("event_description").style.display="none";
 	var xmlhttp=false;
 	if (window.XMLHttpRequest)
 	{// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -18,7 +21,7 @@ window.onload=function(e){
 	if(xmlhttp)
 	{
 		var text="";
-		xmlhttp.open("GET",'event.xml');
+		xmlhttp.open("GET",'event.xml?math=3');
 		xmlhttp.onreadystatechange=function()
 		{
 			if(xmlhttp.readyState==4 && xmlhttp.status==200)
@@ -43,42 +46,22 @@ function enableEvent() {
    document.getElementById("whole_event").style.display="block";
 }
 
-function first_enter(type)
-{
-	document.getElementById("first").style.display = "none";
-	document.getElementById("second").style.display = "block";
-	if(type=="inter")
-	document.getElementById("selection").value="inter";
-	else
-	document.getElementById("selection").value="intra";
-	document.getElementById("back_button").style.display="block";	
-	document.getElementById("event_description").style.display="none";	
-}
 
 function back_operation()
 {
 	if(document.getElementById("selection2").value=="enter")
 	{
-			document.getElementById("first").style.display = "none";
 			document.getElementById("second").style.display = "block";
 			document.getElementById("third").style.display="none";
 			document.getElementById("selection2").value="none";
 			document.getElementById("event_description").style.display="none";
-	}
-	else
-	{
-			document.getElementById("first").style.display = "block";
-			document.getElementById("second").style.display = "none";
-			document.getElementById("third").style.display="none";
-			document.getElementById("back_button").style.display="none";
-			document.getElementById("event_description").style.display="none";	
+			document.getElementById("back_button1").style.display="none";
 	}
 }
 
 function type(entry)
 {
 	var div=document.getElementById("event_list");
-	document.getElementById("first").style.display = "none";
 	document.getElementById("second").style.display = "none";
 	document.getElementById("third").style.display="block";
 	document.getElementById("event_description").style.display="block";	
@@ -87,45 +70,7 @@ function type(entry)
 	document.getElementById("back_button1").style.display="block";
 	var text="";
 	document.getElementById("event_selection").value=entry;
-	if(document.getElementById("selection").value=="inter")
-	{	
-		if(entry=="literary")
 		{
-			text="<ol><li onClick='list_hover(this)'>Debate-Hindi (Single)</li><li onClick='list_hover(this)'>Debate–English (Single)</li><li onClick='list_hover(this)'>Extempore (Group)</li><li onClick='list_hover(this)'>Poetry – Hindi\\ English (Single) </li><li onClick='list_hover(this)'>Youth Parliament(Group)</li><li onClick='list_hover(this)'>Essay Writing – Hindi\\English (Single)</li><li onClick='list_hover(this)'>Travelogue</li><li onClick='list_hover(this)'>Creative Writing–Hindi\\English(Group)</li><li onClick='list_hover(this)'>Quiz (Group)</li></ol>";
-		}
-		else
-		if(entry=="music")
-		{
-			text="<ol><li onClick='list_hover(this)'>Dreamovation</li><li onClick='list_hover(this)'>Qawwali</li><li onClick='list_hover(this)'>Western Vocal</li><li onClick='list_hover(this)'>Hindi Duet</li><li onClick='list_hover(this)'>Instrumental (with using atleast three non-traditional Instruments)</li></ol>";
-		}
-		else
-		if(entry=="art")
-		{
-			text="<ol><li onClick='list_hover(this)'>Comic Strip(Single)</li><li onClick='list_hover(this)'>Rangoli</li><li onClick='list_hover(this)'>Graffi Art (Wall Painting)</li><li onClick='list_hover(this)'>Road Painting</li><li onClick='list_hover(this)'>Poster Making</li><li onClick='list_hover(this)'>Sketching</li><li onClick='list_hover(this)'>Face Painting</li><li onClick='list_hover(this)'>Clay Model</li><li onClick='list_hover(this)'>Stipling</li><li onClick='list_hover(this)'>Banner Making</li></ol>";
-		}
-		else
-		if(entry=="theature")
-		{
-			text="<ol><li onClick='list_hover(this)'>Street Play</li><li onClick='list_hover(this)'>Skit</li><li onClick='list_hover(this)'>Legendary Drama</li><li onClick='list_hover(this)'>Nartya-Natika</li></ol>";
-		}
-		else
-		if (entry=="dance") 
-		{
-			text="<ol><li onClick='list_hover(this)'>Duet</li><li onClick='list_hover(this)'>Bollymania</li><li onClick='list_hover(this)'>Semi-Classical Solo</li><li onClick='list_hover(this)'>Street Dance</li><li onClick='list_hover(this)'>Folk Dance with Live Music</li></ol>";
-		}
-		else
-		if(entry=="photo")
-		{
-			text="<ol><li onClick='list_hover(this)'>SMVDU Cover Shot</li><li onClick='list_hover(this)'>Short Film Making</li><li onClick='list_hover(this)'>Ad Film Making</li><li onClick='list_hover(this)'>Photo presentation</li></ol>";
-		}
-		else
-		if(entry=="special")
-		{
-			text="<ol><li onClick='list_hover(this)'>On the Spot Hidden Talent</li><li onClick='list_hover(this)'>Fashion Show</li><li onClick='list_hover(this)'>Mea Culpa</li></ol>";
-		}
-	}
-	else
-	{
 		if(entry=="literary")
 		{
 			text="<ol><li onClick='list_hover(this)'>Debate</li><li onClick='list_hover(this)'>Quiz</li><li onClick='list_hover(this)'>Group Extempore</li></ol>";	
