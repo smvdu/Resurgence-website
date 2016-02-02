bfe1f1f3cb7a31cf27f378702974e7f76b4e8317
aksh1 <srivastava.sumi3@gmail.com>
2016-02-03 01:41:41 +0530

    Resurgence 16 website initial update

--- connect.php
+++ connect.php
@@ -4,11 +4,11 @@
  *
  * @author Laxmikant Revdikar <laxmikant.4644@gmail.com>
  */
-			$mysql=mysql_connect("localhost","root","")
+			$mysql=mysql_connect("resurgence.smvdu.net.in","","")
 			or die ("Cannot connect to database");
 			if (mysqli_connect_errno())
 			{
 				echo "Failed to connect to MySQL: " . mysqli_connect_error();
 			}
-			$db = mysql_select_db("resurgence",$mysql);
+			$db = mysql_select_db("smvdunet_res2015",$mysql);
 ?>
